6de37dc19a626923b479b206359a29f9f0e88783
Thrasys <tarcisio.wor@gmail.com>
2021-04-24 01:59:14 -0300

    24 as 2

--- lib/home/widgets/chart/chart_widget.dart
+++ lib/home/widgets/chart/chart_widget.dart
@@ -14,37 +14,47 @@ class _ChartWidgetState extends State<ChartWidget>
   late AnimationController _controller;
   late Animation<double> _animation;
 
-  void initAnimation() {
+  void _initAnimation() {
     _controller =
         AnimationController(vsync: this, duration: Duration(seconds: 3));
     _animation = Tween(begin: 0.0, end: widget.percent).animate(_controller);
+    _controller.forward();
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    _initAnimation();
   }
 
   @override
   Widget build(BuildContext context) {
     return Container(
-      width: 80,
-      height: 80,
-      child: Stack(
-        children: [
-          Container(
-            width: 80,
-            height: 80,
-            child: CircularProgressIndicator(
-              strokeWidth: 10,
-              value: 0.75,
-              backgroundColor: AppColors.chartSecondary,
-              valueColor: AlwaysStoppedAnimation<Color>(AppColors.chartPrimary),
-            ),
+        width: 80,
+        height: 80,
+        child: AnimatedBuilder(
+          animation: _animation,
+          builder: (context, _) => Stack(
+            children: [
+              Container(
+                width: 80,
+                height: 80,
+                child: CircularProgressIndicator(
+                  strokeWidth: 10,
+                  value: _animation.value,
+                  backgroundColor: AppColors.chartSecondary,
+                  valueColor:
+                      AlwaysStoppedAnimation<Color>(AppColors.chartPrimary),
+                ),
+              ),
+              Center(
+                child: Text(
+                  "${(_animation.value * 100).toStringAsPrecision(0)}% ",
+                  style: AppTextStyles.heading,
+                ),
+              )
+            ],
           ),
-          Center(
-            child: Text(
-              "75% ",
-              style: AppTextStyles.heading,
-            ),
-          )
-        ],
-      ),
-    );
+        ));
   }
 }
--- lib/home/widgets/score_card/score_card_widget.dart
+++ lib/home/widgets/score_card/score_card_widget.dart
@@ -1,9 +1,14 @@
-import 'package:devquiz/core/app_colors.dart';
-import 'package:devquiz/home/widgets/chart/chart_widget.dart';
 import 'package:flutter/material.dart';
 
+import 'package:devquiz/core/app_colors.dart';
+import 'package:devquiz/home/widgets/chart/chart_widget.dart';
+
 class ScoreCardWidget extends StatelessWidget {
-  const ScoreCardWidget({Key? key}) : super(key: key);
+  final double percent;
+  const ScoreCardWidget({
+    Key? key,
+    required this.percent,
+  }) : super(key: key);
   @override
   Widget build(BuildContext context) {
     return Padding(
@@ -19,7 +24,9 @@ class ScoreCardWidget extends StatelessWidget {
             children: [
               Expanded(
                 flex: 1,
-                child: ChartWidget(),
+                child: ChartWidget(
+                  percent: percent,
+                ),
               ),
               Expanded(
                 flex: 3,
